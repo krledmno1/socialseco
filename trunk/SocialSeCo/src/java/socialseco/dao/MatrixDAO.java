22e5d66e53b8a744b5b4c5cb8cad5cc9a2d18a36
damiansoriano@gmail.com <damiansoriano@gmail.com@2ac72711-7b5b-90d6-93e9-4b98f9fd5bad>
2012-01-24 14:51:28 +0000

    Matrix added

---
+++ trunk/SocialSeCo/src/java/socialseco/dao/MatrixDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socialseco.dao;
+
+import socialseco.util.Matrix;
+
+/**
+ *
+ * @author damian
+ */
+public class MatrixDAO
+    extends GenericDAO<Matrix, Long> {
+    
+}
