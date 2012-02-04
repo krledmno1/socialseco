fdf3747ee3d7f037b571d07066c3038989c7f61b
krledmno1@gmail.com <krledmno1@gmail.com@2ac72711-7b5b-90d6-93e9-4b98f9fd5bad>
2012-02-04 14:38:21 +0000

    added tf_idf for both platforms. EVERYTHING WORKS!!!

--- trunk/SocialSeCo/src/java/socialseco/controller/TFIDFMethod.java
+++ trunk/SocialSeCo/src/java/socialseco/controller/TFIDFMethod.java
@@ -4,6 +4,14 @@
  */
 package socialseco.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import socialseco.model.facebook.FacebookUser;
+import socialseco.model.linkedin.LinkedinUser;
+import socialseco.model.question.Question;
+
 /**
  *
  * @author dd
@@ -12,7 +20,7 @@ public class TFIDFMethod {
     
     public static double computeTFIDFScore(Integer noDoc, String term, String crDoc, String[] allDocs) {
         
-        double tf = LetterPairSimilarity.compareStrings(term, crDoc)/2;
+        double tf = MaxWordSimilarity.compareMinDistanceWords(term, crDoc)/2;
         double noOfDocWithTerm = 1;
         
         for (int i = 0; i < allDocs.length; i++) {
@@ -26,5 +34,28 @@ public class TFIDFMethod {
         
         return tf*idf;
     }
+    
+    public static Map<Long,Double> compare(String q, List<Long> ids, String [] users)
+    {
+         Map<Long,Double> userEvals = new HashMap<Long, Double>();
+         
+         for(int i = 0;i<users.length;i++)
+         {
+             double score=0;
+             if(users[i]!=null && !users[i].isEmpty() && q!=null && !q.isEmpty() )
+             {
+                score = computeTFIDFScore(users.length, q, users[i], users);
+             }
+             userEvals.put(ids.get(i), score);  
+            
+             
+        }
+        return userEvals;
+    }
+    
+
+    
+    
+    
     
 }
